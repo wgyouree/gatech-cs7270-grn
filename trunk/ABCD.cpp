8610b2923a56aaad48fc2d192a6e6e6194ce6f1d
natthu@gmail.com <natthu@gmail.com@f81f8581-20e6-8bb8-ad37-b0517ff08e99>
2011-04-12 23:43:51 +0000

    Part (1) works now. Needs to be run with mem2reg. opt -mem2reg -load ../../../Release/lib/LLVMSimple.so -abc -print-module testFile.bc

--- trunk/ABCD.cpp
+++ trunk/ABCD.cpp
@@ -19,6 +19,7 @@
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include <set>
 #include <vector>
+#include <list>
 #include "llvm/ADT/Twine.h"
 
 using namespace llvm;
@@ -52,98 +53,70 @@ namespace {
 			//for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
 			//	errs() << *I << "\n";
 			//}
-			for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
-				//errs() << *I << "\n";
-
-				//if(isa<CallInst>(*I))
-				//    errs() << "is a call instruction\n";
-
-				if(!isa<GetElementPtrInst>(*I))
-					continue;
-				else {
-					Instruction *inst = &*I;
-					//errs() << *inst << "\n";
-					GetElementPtrInst *GEPI = cast<GetElementPtrInst>(inst);
-					gep_type_iterator GI = gep_type_begin(GEPI), GE = gep_type_end(GEPI);
-
-					uint64_t NumElements = 0;
-					Value *value = NULL;
-
-					while(GI!=GE) {
-						//errs()<< *I << "\n";
-						if (const ArrayType *AT = dyn_cast<ArrayType>(*GI)) {
-							NumElements = AT->getNumElements();
-							errs() << "Number of elements in the array: " << NumElements << "\n";
-
-							value = GEPI->getOperand(2);
-
-							//Works only for array accesses that can be determined in compile time
-							//uint64_t AccessElements = cast<ConstantInt>(GEPI->getOperand(2))->getZExtValue();
-							//errs() << "Accessing element: " << AccessElements << "\n";
-
-						}
-						GI++;
-					}
-
-					if(value==NULL || NumElements==0)
-						continue;
-
-                                        const IntegerType *intType = IntegerType::get(F.getContext(), 32);
-					ConstantInt *consInt = ConstantInt::get(intType, NumElements);
-                                        Constant *cons = cast<Constant>(consInt);
-                                        Value *upperbound = cast<Value>(cons);
-
-					BasicBlock *currBlock = inst->getParent();
-					//errs() << currBlock->getName() << "\n";
-
-					ICmpInst *cmpinst = new ICmpInst(inst, ICmpInst::ICMP_SLT, value, upperbound);
-
-					//BasicBlock::iterator instBegin = currBlock->getInstList().begin();
-					//BasicBlock::iterator instEnd = currBlock->getInstList().end();
-
-					//while(instBegin != instEnd && *instBegin != inst){
-					//	instBegin++;
-					//}
-
-					BasicBlock *newBlock = currBlock->splitBasicBlock(inst, "bounds");
-
-					TerminatorInst *termInst = newBlock->getTerminator();
-					//BasicBlock *otherBlock = termInst->getSuccessor(0);
-					BasicBlock *otherBlock = BasicBlock::Create(F.getContext(), Twine("exitBlock"), &F);
-					Value *one = ConstantInt::get(Type::getInt32Ty(F.getContext()),1);
-					CallInst *exitCall = CallInst::Create(f, one, "", otherBlock);
-					new UnwindInst(F.getContext(), otherBlock);
-
-					//TerminatorInst *termInstc = currBlock->getTerminator();
-					//errs() << *termInstc << "\n";
-
-					BranchInst *branchInst = BranchInst::Create(newBlock, otherBlock, cmpinst);
-
-					llvm::ReplaceInstWithInst(currBlock->getTerminator(), branchInst);
-
-					//termInstc = currBlock->getTerminator();
-					//errs() << *termInstc << "\n";
-					
-					break;
-
-				}
+			std::list<Instruction *> arrayAccessInstList;// = new std::list<Instruction *>();
+			for (Function::iterator BI = F.begin(); BI != F.end(); ++BI){
+				for (BasicBlock::iterator II = (*BI).begin(); II != (*BI).end(); ++II)
+					if (isa<PHINode>(*II))
+						errs() << "Found a phi node\n";
 			}
-			//for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
-			//	errs() << *I << "\n";
-			//}
-			errs() << "Complete\n";
+			for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+				if(isa<GetElementPtrInst>(*I))
+					arrayAccessInstList.push_back(&*I);
+			}
+			std::list<Instruction *>::iterator instIter;
+			for (instIter = arrayAccessInstList.begin(); instIter != arrayAccessInstList.end(); ++instIter){
+				Instruction *inst = *instIter;
+				GetElementPtrInst *GEPI = cast<GetElementPtrInst>(inst);
+				gep_type_iterator GI = gep_type_begin(GEPI), GE = gep_type_end(GEPI);
 
+				uint64_t NumElements = 0;
+				Value *value = NULL;
+
+				while(GI!=GE) {
+					if (const ArrayType *AT = dyn_cast<ArrayType>(*GI)) {
+						NumElements = AT->getNumElements();
+
+						value = GEPI->getOperand(2);
+
+						//Works only for array accesses that can be determined in compile time
+						//uint64_t AccessElements = cast<ConstantInt>(GEPI->getOperand(2))->getZExtValue();
+						//errs() << "Accessing element: " << AccessElements << "\n";
+
+					}
+					GI++;
+				}
+
+				if(value==NULL || NumElements==0)
+					continue;
+
+				const IntegerType *intType = IntegerType::get(F.getContext(), 32);
+				ConstantInt *consInt = ConstantInt::get(intType, NumElements);
+				Constant *cons = cast<Constant>(consInt);
+				Value *upperbound = cast<Value>(cons);
+
+				BasicBlock *currBlock = inst->getParent();
+
+				ICmpInst *cmpinst = new ICmpInst(inst, ICmpInst::ICMP_SLT, value, upperbound);
+
+				BasicBlock *newBlock = currBlock->splitBasicBlock(inst, "bounds");
+
+				TerminatorInst *termInst = newBlock->getTerminator();
+				//BasicBlock *otherBlock = termInst->getSuccessor(0);
+				BasicBlock *otherBlock = BasicBlock::Create(F.getContext(), Twine("exitBlock"), &F);
+				Value *one = ConstantInt::get(Type::getInt32Ty(F.getContext()),1);
+				CallInst *exitCall = CallInst::Create(f, one, "", otherBlock);
+				new UnwindInst(F.getContext(), otherBlock);
+
+				//TerminatorInst *termInstc = currBlock->getTerminator();
+				//errs() << *termInstc << "\n";
+
+				BranchInst *branchInst = BranchInst::Create(newBlock, otherBlock, cmpinst);
+
+				llvm::ReplaceInstWithInst(currBlock->getTerminator(), branchInst);
+			}
+			errs() << "Number of array accesses= " << arrayAccessInstList.size() << "\n";
 			return true;
-
 		}
-
-//		virtual bool doFinalization(Module &M) {
-//			return false;
-//		}
-
-//		virtual void getAnalysisUsage(AnalysisUsage &AU) const {
-//			AU.setPreservesAll();
-//		};
 	};
 
 	char ABCPass::ID = 0;
